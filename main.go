89c5107944a6fe1f70e0dc6334b4d53942d811dc
Luis Felipe Ribeiro Vieira <85707680+Lauiskk@users.noreply.github.com>
2024-04-09 15:25:03 -0300

    main new

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -17,22 +18,29 @@ func main() {
 	}
 
 	db.AutoMigrate(&model.User{})
+	db.AutoMigrate(&model.Tweet{})
 
 	handler := user.Handler{DB: db}
 
 	app := fiber.New()
 
+	app.Use(cors.New())
+
 	api := app.Group("/api")
 
 	api.Get("/", handler.GetAllUsers)
 
-	api.Get("/user/:id", handler.GetUserByID)
+	api.Get("/users/:id", handler.GetUserByID)
+	api.Get("/tweets", handler.GetAllTweets)
+	api.Get("/users/:id/tweets", handler.GetTweetsByUser)
 
-	api.Post("/user", handler.CreateUser)
+	api.Post("/users", handler.CreateUser)
+	api.Post("/users/:id/tweet", handler.CreateTweet)
 
-	api.Put("/user/:id", handler.UpdateUser)
+	api.Put("/users/:id", handler.UpdateUser)
 
-	api.Delete("/user/:id", handler.DeleteUser)
+	api.Delete("/users/:id", handler.DeleteUser)
+	api.Delete("/tweets/:id", handler.DeleteTweet)
 
 	err = app.Listen(":8080")
 	if err != nil {
